5f1109208be2c595f8e04740c57ffdf26ff6d7bc
Stevie Ped <stephen.pederson.au@gmail.com>
2026-05-16 14:32:16 +0930

    Reconfigured setup

--- inst/scripts/data_setup.R
+++ inst/scripts/data_setup.R
@@ -26,24 +26,76 @@
 
 ## Use cached data to ensure it's only downloaded once
 library(BiocFileCache)
-bfc <- BiocFileCache(file.path("data"), ask = FALSE)
+library(tidyverse)
+library(Rsamtools)
+library(rtracklayer)
+library(extraChIPs)
+library(plyranges)
+data_path <- file.path("~", "extraChips_data")
+if (!dir.exists(data_path)) dir.create(data_path)
+bfc <- BiocFileCache(data_path, ask = FALSE)
 
-## Use a helper function to download each folder & unzip
-download_and_unzip <- function(bfc, url, destdir = "data") {
-  ## Only downloads if not already cached
-  path <- bfcrpath(bfc, url)
-  message("Extracting ", basename(url), " ...")
-  unzip(path, exdir = destdir)
-  invisible(destdir)
-}
-
-## The annotation files
-download_and_unzip(
-  bfc, "https://zenodo.org/records/10052122/files/annotations.zip"
+#### Download all/any that are required ####
+url_list <- file.path(
+  "https://zenodo.org/records/10052122/files",
+  c("annotations.zip", "bam.zip", "macs2.zip")
 )
+lapply(url_list, \(x) bfcadd(bfc, x))
 
-## The bam files. NB: these may take 30mins or so
-download_and_unzip(bfc, "https://zenodo.org/records/10052122/files/bam.zip")
+## Extract as zip files
+lapply(bfcinfo(bfc)$rpath, unzip, exdir = data_path)
 
-## The macs2 output
-download_and_unzip(bfc, "https://zenodo.org/records/10052122/files/macs2.zip")
+#### Index Bam Files ####
+## The BamFiles may have conflicting timestamps with the indexes
+## Reindex just for convenience
+file.path(data_path, "bam") |>
+  list.files(pattern = "bam$", full.names = TRUE) |>
+  lapply(indexBam)
+
+#### Setup the annotation files ####
+extdata <- here::here("inst", "extdata", "annotation")
+if (!dir.exists(extdata)) dir.create(extdata, recursive = TRUE)
+## Copy the black & grey lists
+file.copy(
+  file.path(data_path, "annotations", "hg19-blacklist.v2.bed.gz"),
+  file.path(extdata, "hg19-blacklist.v2.bed.gz"), overwrite = TRUE
+)
+file.copy(
+  file.path(data_path, "annotations", "SRR8315192_greylist.bed"),
+  file.path(extdata, "SRR8315192_greylist.bed"), overwrite = TRUE
+)
+sq <- defineSeqinfo("GRCh37", chr = TRUE)
+gtf <- file.path(data_path, "annotations", "gencode.v44lift37.annotation.gtf.gz") |>
+  import.gff(feature.type = c("gene", "transcript", "exon")) |>
+  subset(seqnames %in% seqlevels(sq))
+f <- gzfile(file.path(extdata, "gencode.v44lift37.reduced.gtf.gz"))
+gtf |>
+  select(
+    source, type, score, phase,
+    starts_with("gene"), starts_with("transcript"), starts_with("exon"),
+    -contains(c("status", "support"))
+  ) |>
+  write_gff(f)
+close(f)
+
+#### Copy the peaks across ####
+extdata <- here::here("inst", "extdata", "peaks")
+if (!dir.exists(extdata)) dir.create(extdata, recursive = TRUE)
+file.path(data_path, "macs2") |>
+  list.files(full.names = TRUE, pattern = "narrowPeak$", recursive = TRUE) |>
+  lapply(
+    \(x) {
+      ## Compress for convenience
+      dest <- file.path(extdata, basename(x)) |>
+        paste0(".gz") |>
+        str_replace("E2", "H3K27ac_E2") |>
+        gzfile(open = "w")
+      x |>
+        readLines() |>
+        write(dest)
+      close(dest)
+    }
+  ) |>
+  invisible()
+
+
